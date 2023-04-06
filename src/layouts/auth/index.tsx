88ad1c38dba6a3953d3089484f2ff87bb92a9181
Muhammad <jamiludeensani@gmail.com>
2023-04-06 12:14:15 +0100

    v1.0.0

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -1,5 +1,5 @@
 import Footer from "components/footer/FooterAuthDefault";
-import authImg from "assets/img/auth/auth.png";
+import authImg from "assets/img/auth/delivery-1.svg";
 import { Link, Routes, Route, Navigate } from "react-router-dom";
 import routes from "routes";
 import FixedPlugin from "components/fixedPlugin/FixedPlugin";
@@ -54,10 +54,12 @@ export default function Auth() {
                 </Routes>
                 <div className="absolute right-0 hidden h-full min-h-screen md:block lg:w-[49vw] 2xl:w-[44vw]">
                   <div
-                    className="absolute flex border h-full w-full items-end justify-center bg-cover bg-center lg:rounded-bl-[120px] xl:rounded-bl-[200px]"
+                    className="absolute bg-lightPrimary dark:bg-navy-700 flex h-full w-full items-end justify-center bg-cover bg-center lg:rounded-bl-[120px] xl:rounded-bl-[200px]"
                     // style={{ backgroundImage: `url(${authImg})` }}
                   >
-                  <p className="flex justify-center items-center h-full w-full">Company Logo/Banner Goes here</p>
+                    <div className="flex justify-center items-center h-full w-full">
+                      <img src={authImg} alt="" />
+                  </div>
                   </div>
                 </div>
               </div>
